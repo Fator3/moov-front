ddaf6325506664e8eb432796fe62957057f3851d
Matheus Pessoa <matheus.pessoa16@gmail.com>
2020-05-12 18:28:19 -0300

    [NUD026] implemented about page

--- src/router.js
+++ src/router.js
@@ -8,67 +8,73 @@ import NotFound from './views/NotFound.vue'
 import NetworkIssue from './views/NetworkIssue.vue'
 import FrontPage from './views/FrontPage.vue'
 import Map from './views/Map.vue'
+import About from './views/About.vue'
 
 Vue.use(Router)
 
 const router = new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'search',
-      component: FrontPage,
-      props: true
-    },
-    {
-      path: '/map',
-      name: 'map',
-      component: Map,
-      props: true
-    },
-    {
-      path: '/list',
-      name: 'list',
-      component: PropertyList,
-      props: true
-    },
-    {
-      path: '/property/:id/:property',
-      name: 'property-show',
-      component: PropertyShow,
-      props: true
-    },
-    {
-      path: '/create',
-      name: 'create',
-      component: PropertyCreate
-    },
-    {
-      path: '/404',
-      name: '404',
-      component: NotFound,
-      props: true
-    },
-    {
-      path: '/network-issue',
-      name: 'network-issue',
-      component: NetworkIssue
-    },
-    {
-      path: '*',
-      redirect: { name: '404', params: { resource: 'page' } }
-    }
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [{
+            path: '/',
+            name: 'search',
+            component: FrontPage,
+            props: true
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About,
+            props: true
+        },
+        {
+            path: '/map',
+            name: 'map',
+            component: Map,
+            props: true
+        },
+        {
+            path: '/list',
+            name: 'list',
+            component: PropertyList,
+            props: true
+        },
+        {
+            path: '/property/:id/:property',
+            name: 'property-show',
+            component: PropertyShow,
+            props: true
+        },
+        {
+            path: '/create',
+            name: 'create',
+            component: PropertyCreate
+        },
+        {
+            path: '/404',
+            name: '404',
+            component: NotFound,
+            props: true
+        },
+        {
+            path: '/network-issue',
+            name: 'network-issue',
+            component: NetworkIssue
+        },
+        {
+            path: '*',
+            redirect: { name: '404', params: { resource: 'page' } }
+        }
+    ]
 })
 
 router.beforeEach((routeTo, routeFrom, next) => {
-  NProgress.start()
-  next()
+    NProgress.start()
+    next()
 })
 
 router.afterEach(() => {
-  NProgress.done()
+    NProgress.done()
 })
 
 export default router
---
+++ src/views/About.vue
@@ -0,0 +1,286 @@
+<template>
+  <v-container class="ma-0 pa-0 wrapper" fluid>
+    <v-sheet id="headline" class="display-2">
+      <span id="spacing-text" class="text-style">A nossa missão é te possibilitar viver<br />
+        da melhor forma que pode!<br />
+        Do seu jeito. <br />
+        De acordo com suas necessidades.</span>
+      <span class="text-style secondary--text">O tempo é agora. Bora viver? </span>
+    </v-sheet>
+    <v-sheet class="display-2">
+      <v-row no-gutters>
+        <v-col class="mb-6">
+          <span class="text-style-secondary">Somos uma plataforma<br />
+            desenvolvida para você<br />
+            encontrar o melhor lugar <br />
+            para viver!
+          </span>
+        </v-col>
+        <v-col class="mb-6 d-flex justify-content-end" style="padding-bottom:20px;">
+          <div class="img-container">
+            <img src="../assets/images/about-girl.png" class="img-align-left">
+            <div class="rect-after" ></div>
+          </div>
+        </v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row no-gutters >
+        <v-col class="md-2 pa-1 d-flex justify-content-center">
+          <div>
+            <img class="img-size" src="../assets/images/icon-confort.png">
+            <span class="text-icon">Conforto</span>
+          </div>
+        </v-col>
+        <v-col class="md-2 pa-1 d-flex justify-content-center">
+          <div>
+            <img class="img-size" src="../assets/images/icon-safety.png">
+            <span class="text-icon">Segurança</span>
+          </div>
+        </v-col>
+        <v-col class="md-2 pa-1 d-flex justify-content-center">
+          <div>
+            <img class="img-size" src="../assets/images/icon-key.png">
+            <span class="text-icon">Facilidade</span>
+          </div>
+        </v-col>
+        <v-col class="md-2 pa-1 d-flex justify-content-center">
+          <div>
+            <img src="../assets/images/icon-home.png" class="img-size" >
+            <span class="text-icon">Novo lar</span>
+          </div>
+        </v-col>
+      </v-row>
+      <v-divider></v-divider>
+      <v-row no-gutters="">
+        <v-col class="md-6 pa-6">
+          <div class="rect-before">
+            <div class="img-container-over ">
+              <img src="../assets/images/couple.png" class="img-align-top">
+            </div>
+          </div> 
+        </v-col>
+        <v-col class="md-6 pa-6">
+          <span class="text-style-secondary">
+            Nosso objetivo é ajudar você <br />
+            a encontrar o imóvel ideal, no <br />
+            lugar que tem a sua cara!
+          </span>
+        </v-col>
+      </v-row>
+    </v-sheet>
+    <v-divider style="margin-top: 6%; margin-bottom:4%;"></v-divider>
+    <v-row no-gutters class="justify-content-center">
+      <span class="text-style-tertiary">
+        <span>Então,</span> qual vida você quer viver?
+      </span>
+    </v-row>
+    <v-divider style="margin-top: 4%;"></v-divider>
+    <v-row no-gutters class="justify-content-center text-style-tertiary spacing-row">
+      <span class="justify-content-center">Fundadores</span>
+    </v-row>
+    <v-row no-gutters class="justify-content-center spacing-row">
+      <v-col class="d-flex justify-content-center">
+        <div>
+            <img class="img-founders" src="../assets/images/handall.png">
+            <span class="text-name-founders d-flex justify-content-center">Handall</span>
+            <div class="d-flex justify-content-center">
+              <span class="text-part-founders">Co-Founder</span>
+            </div>
+        </div>
+      </v-col>
+      <v-col class="d-flex justify-content-center">
+        <div>
+          <img class="img-founders img-spacing" src="../assets/images/andre.png">
+          <span class="text-name-founders">André Filipe Freitas</span>
+          <div class="d-flex justify-content-center">
+            <span class="text-part-founders">Founder</span>
+          </div>
+        </div>
+      </v-col>
+      <v-col class="d-flex justify-content-center">
+        <div>
+          <img class="img-founders img-spacing" src="../assets/images/alexandre.png">
+          <span class="text-name-founders">Alexandre Martins</span>
+          <div class="d-flex justify-content-center">
+            <span class="text-part-founders">Co-Founder</span>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.wrapper {
+  width: 100% !important;
+  max-width: 100% !important;
+  position: relative;
+}
+
+#headline {
+  width: 100% !important;
+  max-width: 100% !important;
+  min-height: 770px;
+  background: linear-gradient(
+      90deg,
+      rgba(127, 86, 195, 0.9) 0%,
+      rgba(81, 194, 247, 0.9) 121.21%
+    ),
+    url('../assets/images/frontpage-background.png') no-repeat;
+  background-size: cover;
+  z-index: 1;
+}
+.text-style {
+  font-family: Roboto, sans-serif;
+  font-style: bold;
+  font-size: 35px;
+  line-height: 41px;
+  display: flex;
+  align-items: center;
+  text-align: justify;
+  letter-spacing: 1.02px;
+  color: white;
+  margin-left: 5%;
+}
+#spacing-text {
+  margin-top: 8%;
+}
+.text-style-secondary {
+  font-family: Roboto, sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 35px;
+  line-height: 41px;
+  display: flex;
+  align-items: center;
+  text-align: justify;
+  letter-spacing: 1.02px;
+  margin-left: 10%;
+  margin-top: 20%;
+  color: #7F56C3;
+}
+.bottom-text {
+  font-weight: normal;
+  font-size: 22px;
+  line-height: 26px;
+  letter-spacing: 1.48194px;
+}
+.bottom {
+  width: 100%;
+  max-width: 100% !important;
+  padding: 40px 80px;
+}
+.img-container {
+  /* float: right; */
+  margin-right: 10%;
+  width: 380px;
+  height: 430px;
+  background-color: #7F56C3;
+  position: relative;
+}
+.rect-after {
+  position: absolute;
+  width: 380px;
+  height: 430px;
+  border: 2px solid #34B6F2;
+  top: 20px;
+  right: 20px;
+}
+.rect-before {
+  position: relative;
+  width: 380px;
+  height: 430px;
+  border: 2px solid #34B6F2;
+  top: 50px;
+  left: 15%;
+}
+.img-container-over {
+  bottom: 20px;
+  right: 20px;
+  width: 380px;
+  height: 430px;
+  background-color: #7F56C3;
+  position: absolute;
+}
+.img-align-top {
+  padding-top: 85px; 
+}
+.img-align-left {
+  padding-top: 35px;
+  padding-left: 15px;
+}
+.img-size {
+  width: 31px;
+  height: 31px;
+  display: flex;
+  margin-left: auto;
+  margin-right: auto;
+}
+.v-divider {
+  margin-bottom: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 40%;
+}
+.text-icon {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 35px;
+  display: flex;
+  letter-spacing: 1.02px;
+  margin-left: 20px;
+  color: #34B6F2;
+}
+.text-style-tertiary {
+  color: #34B6F2;
+  font-style: bold;
+  font-size: 35px;
+  font-family: Roboto;
+  font-weight: 500;
+  margin: auto;
+}
+.text-style-tertiary span {
+  color:#7F56C3;
+  font-size: 35px;
+}
+.spacing-row {
+  margin-top: 3%;
+  margin-bottom: 3%;
+}
+.img-founders {
+  width: 190px;
+  height: 190px;
+}
+.img-spacing {
+    margin-left: 15%;
+}
+.text-name-founders {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 30px;
+  line-height: 35px;
+  display: flex;
+  align-items: center;
+  letter-spacing: 1.02px;
+  padding-top: 20px;
+  color: #454646;
+}
+.text-part-founders {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 23px;
+  /* display: flex; */
+  /* align-items: center; */
+  letter-spacing: 1.02px;
+  color: #7F56C3;
+}
+</style>
